ae2d5ca60fb509b918a93a4583c82b85aa184cd2
Gokul <gokulyc@gmail.com>
2020-11-12 14:52:02 +0530

    3 frontent react || django integration done

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig', ## added
     'rest_framework', ## added
+    'frontend.apps.FrontendConfig', ## added
 ]
 
 MIDDLEWARE = [
